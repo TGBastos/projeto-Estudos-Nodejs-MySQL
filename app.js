eea6abd084c3134c0a1b995e3c4888a81803e445
TGBastos <dm.thiagogb@hotmail.com>
2024-10-16 23:08:46 -0300

    impelentando função de cadastro e manipulação dos dados através das rotas

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const mysql = require('mysql2');
 
 // Importar Express-handlebars
 const { engine } = require('express-handlebars');
+// App
+const app = express();
+
+// Manipulação de dados via rotas
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 // Configuração de conexeção
 const conexecao = mysql.createConnection({
@@ -21,8 +27,6 @@ conexecao.connect(function (erro) {
     console.log('Conexao efetuada com sucesso!');
 })
 
-// App
-const app = express();
 
 // Adicionar bootstrap
     app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
@@ -36,9 +40,14 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 // Rota principal
-
 app.get('/', function (req, res) {
     res.render('formulario');
+});
+
+// Rota de Cadastro 
+app.post('/cadastrar', function(req, res){
+    console.log(req.body);
+    res.end();
 });
 
 // Servidor
